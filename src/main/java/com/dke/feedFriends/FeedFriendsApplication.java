ab924d9a0d887bf20ccddf6ebb199aff6907ef1e
PhilippJKU <p.engelbrechtsmueller@gmx.at>
2021-04-23 17:44:18 +0200

    Add FeedFriends

---
+++ src/main/java/com/dke/feedFriends/FeedFriendsApplication.java
@@ -0,0 +1,13 @@
+package com.dke.feedFriends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedFriendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedFriendsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dke/feedFriends/FeedFriendsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dke.feedFriends;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedFriendsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
